07e9f5f6b4db68481ff69a29bf99e36a68fb59fb
tirth <tirth5524@gmail.com>
2024-06-29 13:45:02 +0530

    add E health care managment System  Project

--- typro/typro/settings.py
+++ typro/typro/settings.py
@@ -79,17 +79,6 @@ WSGI_APPLICATION = 'typro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'health_admin12',
-        'USER':'root',
-        'host':'localhost',
-        'PASSWORD':'tirth@5524',
-        
-    }
-}
 
 
 
@@ -152,16 +141,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'prajapatitirth031@gmail.com'
-EMAIL_HOST_PASSWORD ='hobm vpnx oxvw kjig'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'hobm vpnx oxvw kjig
-
-RZP_API_KEY='rzp_test_HbVeg2ZkY2GmUI'
-RZP_API_SECRET='zjFcPssv4soJgmMOgqnFAfW1'
- 
